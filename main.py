56128592302a496b54410b9e0d8ff660720f26e0
anto4 <anto.hosek@gmail.com>
2026-01-11 21:19:26 +0100

    better website

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from pydub import AudioSegment
 from pydub.silence import *
 
 languages_dir = 'languages'
-output_audio_dir = 'site/output_audio'
+output_audio_dir = '_site/output_audio'
 tmp_dir = 'tmp'
 
 # Ensure output directories exist
@@ -25,10 +25,13 @@ html_content = """<!doctype html>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width" />
     <title>Audio language courses</title>
+    <link rel="stylesheet" href="styles.css">
   </head>
-<body>"""
+<body>
+<h1>Audio language courses</h1>
+<p>Learn the basics of foreign languages, to communicate with locals.</p>"""
 
-source_languages = [["english", "tts_models/en/ljspeech/vits"], ["francais", "tts_models/fr/css10/vits"]]
+source_languages = [["english", "tts_models/en/ljspeech/vits"]] #, ["francais", "tts_models/fr/css10/vits"]]
 
 # Iterate and parse each json file
 for json_file in json_files:
@@ -48,6 +51,9 @@ for json_file in json_files:
             silence = AudioSegment.silent(duration=1000) # 1 second silence
 
             for i, sentence in enumerate(data['sentences']):
+                if source_language[0] not in sentence:
+                    break
+
                 translated_sentence = sentence[source_language[0]]
                 target_audio_filename = sentence["audio_file"]
 
@@ -78,16 +84,17 @@ for json_file in json_files:
             # Export the final course audio
             output_filename = f"{source_language[0]}_{language_slug}_course.wav"
             output_filepath = os.path.join(output_audio_dir, output_filename)
+            site_output_filepath = os.path.join('output_audio', output_filename)
             course_audio.export(output_filepath, format="wav")
             print(f"Successfully created course audio: {output_filepath}")
 
             html_content += f"""<div class="audio-item">
-                <h2>{source_language[0]} to {data['language_name']}</h2>
+                <h2>{source_language[0].capitalize()} to {data['language_name']} course</h2>
                 <audio controls>
-                    <source src="{output_filepath}" type="audio/mpeg">
+                    <source src="{site_output_filepath}" type="audio/mpeg">
                     Your browser does not support the audio element.
                 </audio>
-                <a href="{output_filepath}" download>Download course</a>
+                <a href="{site_output_filepath}" download>Download course</a>
             </div>"""
 
 print("\nAll courses processed.")
@@ -95,5 +102,5 @@ print("\nAll courses processed.")
 html_content += """</body>
 </html>"""
 
-with open("site/index.html", 'w', encoding='utf-8') as f:
+with open("_site/index.html", 'w', encoding='utf-8') as f:
     f.write(html_content)
