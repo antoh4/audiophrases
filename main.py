a70acb9a1c3fadf1b41de91ca7c890080ff21bc8
anto4 <anto.hosek@gmail.com>
2026-01-26 17:50:05 +0100

    divide the site in multiple pages

--- main.py
+++ main.py
@@ -2,18 +2,23 @@ import json
 import os
 from pydub import AudioSegment
 from pydub.silence import *
+import shutil
 
 languages_dir = 'languages'
-output_audio_dir = '_site/course'
+output_audio_dir = '_site/audio_courses'
+output_pages_dir = '_site/courses'
 tmp_dir = 'tmp'
 
 # Ensure output directories exist
 os.makedirs(output_audio_dir, exist_ok=True)
 os.makedirs(tmp_dir, exist_ok=True)
 
+# we remove the old pages and recreate the folder. as we regenerate the pages
+shutil.rmtree(output_pages_dir, ignore_errors=True)
+os.makedirs(output_pages_dir, exist_ok=True)
+
 # List all json files in the languages directory
 json_files = sorted([f for f in os.listdir(languages_dir) if f.endswith('.json')])
-
 
 print(f"Found {len(json_files)} JSON files in '{languages_dir}': {json_files}\n")
 
@@ -26,7 +31,7 @@ html_content = """<!doctype html>
     <link rel="stylesheet" href="styles.css">
   </head>
 <body>
-<h1>Language learning audio phrases</h1>
+<a href="/"><h1>Language learning audio phrases</h1></a>
 <p>Learn the basics of foreign languages with audio phrases, to communicate more easily around the world and make people smile.</p>
 <p>Listen to the phrase in the target language, repeat it, listen to the translation, and repeat the phrase again and think of the link between the phrase and the translation.</p>
 <p>The courses are made to be listened a lot of times (for instance while going on walks), make your memory work (mix of repetition and active recall), and get used to the sonorities of the language.</p>"""
@@ -52,15 +57,18 @@ for json_file in json_files:
     
     possible_translations = {"en-US": [AudioSegment.empty(), 0], "fr-FR": [AudioSegment.empty(), 0]}
 
-    language_name = data['language_name']
-    language_slug = data['slug']
-    print(f"\n--- Processing {language_name} ({json_file}) ---\n")
+    course_name = data['course_name']
+    print(f"\n--- Processing {course_name} ({json_file}) ---\n")
 
-    html_content += f"""<hr><h2>{language_name} courses</h2>"""
+    html_content += f"""<hr><h2>{course_name} courses</h2><ul>"""
 
-    pre_silence = AudioSegment.silent(duration=3000) # ms
+    pre_silence = AudioSegment.silent(duration=2500) # ms
     post_silence = AudioSegment.silent(duration=2500) # ms
-    small_silence = AudioSegment.silent(duration=2000) # ms
+    small_silence = AudioSegment.silent(duration=2500) # ms
+
+    page_url = f"courses/{json_file}.html"
+
+    
 
     for i, sentence in enumerate(data['sentences']):
         target_audio_filename1 = sentence['sentence'][1]
@@ -68,7 +76,6 @@ for json_file in json_files:
 
         if target_audio_filename1 != "" and target_audio_filename2 != "":
 
-            # Assuming target audio files are in a subdirectory named after the slug within languages_dir
             target_audio_path1 = os.path.join(languages_dir, "individual_audios", target_audio_filename1)
             target_audio_path2 = os.path.join(languages_dir, "individual_audios", target_audio_filename2)
 
@@ -105,27 +112,45 @@ for json_file in json_files:
             tr_language_name = LANG_NAMES.get(language, language)
 
             # Export the final course audio
-            output_filename = f"{language_slug}_{tr_language_name}_course.mp3"
+            output_filename = f"{json_file}_{tr_language_name}_course.mp3"
             output_filepath = os.path.join(output_audio_dir, output_filename)
             site_output_filepath = os.path.join('course', output_filename)
             course_audio = course_audio.set_frame_rate(22050)
             course_audio = course_audio.set_sample_width(2)
             course_audio.export(output_filepath, format="mp3", bitrate="128k")
-            print(f"Successfully created course audio: {output_filepath}")
 
-            html_content += f"""<div class="audio-item">
-                <h3>{tr_language_name} → {language_name}</h3>
+            html_content += f"""<li><a href="/{page_url}">{course_name} for {tr_language_name} speakers</a> ({possible_translations[language][1]} phrases)</li>"""
+
+            page_html_content = f"""<!doctype html>
+                <html lang="en">
+                <head>
+                    <meta charset="utf-8" />
+                    <meta name="viewport" content="width=device-width" />
+                    <title>Language learning audio phrases</title>
+                    <link rel="stylesheet" href="styles.css">
+                </head>
+                <body>
+                <a href="/"><h1>Language learning audio phrases</h1></a>
+                <h2>{course_name} for {tr_language_name} speakers.</h2>
+                <div class="audio-item">
                 <audio controls>
                     <source src="{site_output_filepath}" type="audio/mpeg">
                     Your browser does not support the audio element.
                 </audio>
                 <p>({possible_translations[language][1]} phrases)</p>
-                </div>
+                </div></body>
+                </html>
                 """
+            
+            with open(f"_site/{page_url}", 'w', encoding='utf-8') as f:
+                f.write(page_html_content)
+            
+            
+            print(f"Successfully created course audio: {output_filepath}")
 
 print("\nAll courses processed.")
 
-html_content += """<br><hr><small><p><a href="https://codeberg.org/anto4/audiophrases/">Open-source website</a></p></small></body>
+html_content += """</ul><br><hr><small><p><a href="https://codeberg.org/anto4/audiophrases/">Open-source website</a></p></small></body>
 </html>"""
 
 with open("_site/index.html", 'w', encoding='utf-8') as f:
